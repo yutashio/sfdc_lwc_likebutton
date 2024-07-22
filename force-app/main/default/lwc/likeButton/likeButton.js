d17a353644dfcf7ef7977f8f96b0dcf06c483c9c
yutashio <yushiotadev@gmail.com>
2024-07-22 18:44:35 +0900

    first commit

---
+++ force-app/main/default/lwc/likeButton/likeButton.js
@@ -0,0 +1,94 @@
+import { LightningElement, api, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import userId from "@salesforce/user/Id";
+import init from '@salesforce/apex/LikeButtonController.init';
+import onClickLike from '@salesforce/apex/LikeButtonController.onClickLike';
+import onClickCancelLike from '@salesforce/apex/LikeButtonController.onClickCancelLike';
+
+export default class LikeButton extends LightningElement {
+
+	//レコードID
+	@api recordId;
+	//いいね件数
+	likeCount;
+	//ボタン切り替え用
+	alreadyLiked = false;
+	//スピナー表示用
+	spinner = false;
+	//ログイン中ユーザID
+	loginUserId = userId;
+	//ユーザーアイコンの表示制御用
+	overLimit = false;
+
+	connectedCallback(){
+		this.spinner = true;
+
+		init({recordId: this.recordId, userId: this.loginUserId})
+		.then(result => {
+			this.likeCount = result.totalLikeCount;
+			this.alreadyLiked = result.alreadyLiked;
+			this.likeList = result.displayLikeResult;
+			this.overLimit = result.overLimit;
+			this.spinner = false;
+		})
+		.catch(error => {
+			this.dispatchEvent(
+				new ShowToastEvent({
+					title: 'エラーが発生しました。',
+					message: error.body.message,
+					variant: 'error',
+					mode: 'sticky'
+				})
+			);
+			this.spinner = false;
+		});
+	}
+
+	onClickLikeButton() {
+		this.spinner = true;
+
+		onClickLike({recordId: this.recordId, userId: this.loginUserId})
+		.then(result => {
+			this.likeCount = result.totalLikeCount;
+			this.alreadyLiked = result.alreadyLiked;
+			this.likeList = result.displayLikeResult;
+			this.overLimit = result.overLimit;
+			this.spinner = false;
+		})
+		.catch(error => {
+			this.dispatchEvent(
+				new ShowToastEvent({
+					title: 'エラーが発生しました。',
+					message: error.body.message,
+					variant: 'error',
+					mode: 'pester'
+				})
+			);
+			this.spinner = false;
+		});
+	}
+
+	onClickCancelLikeButton() {
+		this.spinner = true;
+
+		onClickCancelLike({recordId: this.recordId, userId: this.loginUserId})
+		.then(result => {
+			this.likeCount = result.totalLikeCount;
+			this.alreadyLiked = result.alreadyLiked;
+			this.likeList = result.displayLikeResult;
+			this.overLimit = result.overLimit;
+			this.spinner = false;
+		})
+		.catch(error => {
+			this.dispatchEvent(
+				new ShowToastEvent({
+					title: 'エラーが発生しました。',
+					message: error.body.message,
+					variant: 'error',
+					mode: 'pester'
+				})
+			);
+			this.spinner = false;
+		});
+	}
+}
